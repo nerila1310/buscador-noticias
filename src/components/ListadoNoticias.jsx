4696fb33b28588c457ffc61ea2e2fc856c5fbca9
Neri Laredo <nerila1310@gmail.com>
2021-08-03 13:29:28 -0500

    Documentando con Proptypes y finalizando el proyecto

--- src/components/ListadoNoticias.jsx
+++ src/components/ListadoNoticias.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Noticia from './Noticia';
+import PropTypes from 'prop-types';
 
 const ListadoNoticias = ({noticias}) => (
     <div className="row">
@@ -12,5 +13,10 @@ const ListadoNoticias = ({noticias}) => (
 
     </div>
 );
+
+ListadoNoticias.propTypes = {
+    noticias: PropTypes.array.isRequired
+}
+
 
 export default ListadoNoticias;
