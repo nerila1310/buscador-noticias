931395adafcf79aaa1855109b258cbe4a9f47b0a
Neri Laredo <nerila1310@gmail.com>
2021-08-03 13:02:43 -0500

    Creando un listado de noticias

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Fragment, useState, useEffect} from 'react';
 
 import Formulario from './components/Formulario';
 import Header from './components/Header';
+import ListadoNoticias from './components/ListadoNoticias';
 
 function App() {
 
@@ -30,6 +31,10 @@ function App() {
       <div className="container white">
         <Formulario 
           guardarCategoria={guardarCategoria}
+        />
+
+        <ListadoNoticias
+          noticias={noticias} 
         />
 
       </div>
